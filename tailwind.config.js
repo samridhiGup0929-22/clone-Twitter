2a4ddc658c52f6e50542e36874dce810c8c2bd3c
Samridhi Gupta <guptasamridhi98@gmail.com>
2026-02-26 20:44:20 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.html"],
+    theme: {
+        extend: {
+            screens: {
+                        'sm': '640px',
+                        'md': '768px',
+                        'lg': '1024px',
+                        'xl': '1280px',
+            },
+            fontFamily: {
+                twitter: ['Inter', 'system-ui', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
